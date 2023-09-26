57dae2eed838c781116630f21b9784bc9cf8cd66
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2023-09-26 14:24:35 -0400

    initial navigation functions

--- App.js
+++ App.js
@@ -4,10 +4,21 @@ import{Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions} fr
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import FillerImage from './assets/penguin.png';
 
-export default function App() {
-  return (
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen(){
+  return(
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+function MainScreen({navigation}){
+  return(
     <SafeAreaView
-    style={{
+      style={{
         flex:1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -20,12 +31,32 @@ export default function App() {
     </View>
     
     {/* <View style = {{flex:1, justiftContent:'Center', alignItems:'center'}}></View> */}
-    <TouchableOpacity style={{backgroundColor: '#AD40AF', padding:20, width:'90%', borderRadius:5,flexDirection:'row', justifyContent:'space-between', marginTop: 30}}>
+    <TouchableOpacity 
+      onPress={()=>navigation.navigate('Home')}
+      style={{backgroundColor: '#AD40AF', padding:20, width:'90%', borderRadius:5,flexDirection:'row', justifyContent:'space-between', marginTop: 30}}>
       {/*Customized font themes in react-native.config.js, couldn't figure out who to rebuild*/}
-      <Text style = {{fontWeight:'bold', fontSize:18, color:'#fff', fontStyle:'italic'}}> I want to see cool things! 🍔</Text>
+      <Text style = {{fontWeight:'bold', fontSize:18, color:'#fff', fontStyle:'italic'}}> I want to see cool things!</Text>
       <MaterialIcons name="arrow-forward-ios" size={22} color="#fff"></MaterialIcons>
     </TouchableOpacity>
-  </SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+/* function App() {
+  return <HomeScreen/>
+}
+
+export default App; */
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
