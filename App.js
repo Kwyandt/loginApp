465d2feff463c614b2c15521a152468c5894af6d
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2023-09-26 13:54:16 -0400

    finished splash page

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 
 import{Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FillerImage from './assets/penguin.png';
 
 export default function App() {
   return (
@@ -12,11 +12,15 @@ export default function App() {
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: "#fff"}}>
+
+    {/* Future: figure otu the svg thing */}
+    <Image source={FillerImage} alt="penguin"/>
     <View>
       <Text style={{fontSize:30, fontWeight:'bold', color: '#20315f'}}>Something 4 Everyone</Text>
-      {/* <StatusBar style="auto" /> */}
     </View>
-    <TouchableOpacity style={{backgroundColor: '#AD40AF', padding:20, width:'90%', borderRadius:5,flexDirection:'row', justifyContent:'space-between'}}>
+    
+    {/* <View style = {{flex:1, justiftContent:'Center', alignItems:'center'}}></View> */}
+    <TouchableOpacity style={{backgroundColor: '#AD40AF', padding:20, width:'90%', borderRadius:5,flexDirection:'row', justifyContent:'space-between', marginTop: 30}}>
       {/*Customized font themes in react-native.config.js, couldn't figure out who to rebuild*/}
       <Text style = {{fontWeight:'bold', fontSize:18, color:'#fff', fontStyle:'italic'}}> I want to see cool things! 🍔</Text>
       <MaterialIcons name="arrow-forward-ios" size={22} color="#fff"></MaterialIcons>
@@ -25,11 +29,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
